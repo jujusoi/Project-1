121eacf93b26871448f4124f57927f06ad18ca4e
jujusoi <jalxmcdonald@hotmail.com>
2023-07-24 12:28:47 +0930

    added save logic and remove save logic. needs init.

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -3,6 +3,7 @@ var foodImage = document.querySelector("#recipe-image");
 var foodDescription = document.querySelector("#recipe-description");
 var recipeLink = document.querySelector("#recipe-link");
 var recipeNutrientList = document.querySelector("#nutrient-list");
+var saveButton = document.querySelector("#save-recipe-button");
 
 function init() {
     var foodName = window.localStorage.getItem("RecipeName");
@@ -12,7 +13,7 @@ function init() {
     var firstLetter = splitDesc[0].charAt(0);
     foodDescription.textContent = determineaOrAn(firstLetter) + " " + splitDesc[0] + " " + splitDesc[1] + " meal, typically eaten for " + splitDesc[2] + ".";
     var recipeLinkItem = window.localStorage.getItem("RecipeLink");
-    recipeLink.textContent = recipeLinkItem;
+    recipeLink.textContent = "Link to recipe";
     recipeLink.href = recipeLinkItem;
     foodTitle.textContent = foodName;
     foodImage.setAttribute("src", foodImgSrc);
@@ -43,6 +44,7 @@ function makeNutrientList(ingredients) {
         var cutDecimals = nutrientAmount.toFixed(1);
 
         newListItem.textContent = nutrientLabel + ": " + cutDecimals + nutrientUnit;
+        newListItem.setAttribute("class", "nutrient-li");
         recipeNutrientList.appendChild(newListItem);
     }
 
@@ -65,3 +67,61 @@ function determineaOrAn(startingLetter) {
 }
 
 init();
+
+saveButton.addEventListener("click", saveObject);
+
+function saveObject() {
+    if (saveButton.getAttribute("data-activated") === "no") {
+        saveButton.setAttribute("data-activated", "yes");
+        saveButton.textContent = "Remove";
+        var savedObjArray = JSON.parse(window.localStorage.getItem("SavedFoods")) || [];
+        var savedObject = {
+            foodName: document.querySelector("#recipe-title").textContent,
+            foodDescription: document.querySelector("#recipe-description").textContent,
+            foodImage: document.querySelector("#recipe-image").getAttribute("src"),
+            foodIngredients: getIngr(),
+            foodNutrients: getNutr(),
+            foodRecipe: recipeLink.getAttribute("href")
+        };
+        savedObjArray.push(savedObject);
+        window.localStorage.setItem("SavedFoods", JSON.stringify(savedObjArray));
+    } else {
+        saveButton.setAttribute("data-activated", "no");
+        saveButton.textContent = "Save For Later";
+        var info = JSON.parse(window.localStorage.getItem("SavedFoods"));
+        var recipeTitle =  document.querySelector("#recipe-title");
+        for (var i = 0; i < info.length; i++) {
+            var foodName = info[i].foodName;
+            console.log(foodName);
+            if (foodName === recipeTitle.textContent) {
+                info.splice(i, 1);
+                window.localStorage.setItem("SavedFoods", JSON.stringify(info));
+            }
+        }
+    }
+}
+
+function getIngr() {
+    var ingredientList = document.querySelector("#recipe-list");
+    var ingredientChildren = ingredientList.childElementCount;
+    var ingredientArray = [];
+    for (var i = 0; i < ingredientChildren; i++) {
+        var ingredientTextContent = ingredientList.children[i].textContent;
+        ingredientArray.push(ingredientTextContent);
+    } return ingredientArray;
+}
+function getNutr() {
+    var nutriList = document.querySelector("#nutrient-list");
+    var nutriChildren = nutriList.childElementCount;
+    var nutriArray = [];
+    for (var i = 0; i < nutriChildren; i++) {
+        var nutriTextContent = nutriList.children[i].textContent;
+        nutriArray.push(nutriTextContent);
+    } 
+    return nutriArray;
+}
+
+function grabSavedObjs(saved) {
+    if (saved === null) {
+    }
+}
