0ec998c5733d6d22b3f9539f85495fc48174d52a
jujusoi <jalxmcdonald@hotmail.com>
2023-07-25 20:10:19 +0930

    combined js functionality

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
+// Function to handle the recipe search
 function searchRecipes(query) {
-    const appId = '9267bdde';
-    const appKey = '4529adbb76333a4f1081930cfc2af312';
-    const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`;
+    const appId = '9267bdde'; // App ID
+    const appKey = '4529adbb76333a4f1081930cfc2af312'; // API key
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`; // URL
 
     fetch(url)
-    .then(response => response.json())
+    .then(response => response.json()) // Parse response
     .then(data => {
         if (data.count > 0) {
             console.log(data)
             
             const recipesList = document.getElementById('recipes-list');
-            recipesList.innerHTML = '';
+            recipesList.innerHTML = ''; // Clear previous results
             data.hits.forEach(hit => {
                 const recipe = hit.recipe;
 
-                const recipeCard = document.createElement('div');
-                recipeCard.classList.add('recipe-card');
+                // Create HTML elements for the recipe details
+                const recipeCard = document.createElement('div'); // Create recipe card
+                recipeCard.classList.add('recipe-card'); // Add class
 
-                const recipeName = document.createElement('h3');
-                recipeName.textContent = recipe.label;
-                recipeCard.appendChild(recipeName);
+                const recipeName = document.createElement('h3'); // Create recipe name
+                recipeName.textContent = recipe.label; // Add recipe name
+                recipeCard.appendChild(recipeName); // Add recipe name to recipe card
 
-                const recipeURL = document.createElement('a');
-                recipeURL.href = recipe.url;
-                recipeURL.textContent = recipe.url;
-                recipeCard.appendChild(recipeURL);
+                const recipeURL = document.createElement('a');// Create recipe URL
+                recipeURL.href = recipe.url;// Add recipe URL
+                recipeURL.textContent = recipe.url;// Add recipe URL
+                recipeCard.appendChild(recipeURL);// Add recipe URL to recipe card
 
-                const recipeImage = document.createElement('img');
-                recipeImage.src = recipe.image;
-                recipeCard.appendChild(recipeImage);
+                const recipeImage = document.createElement('img');// Create recipe image
+                recipeImage.src = recipe.image; // Add recipe image
+                recipeCard.appendChild(recipeImage);// Add recipe image to recipe card
 
-                const recipeIngredients = document.createElement('p');
+                const recipeIngredients = document.createElement('p');// Create recipe ingredients
                 recipeIngredients.textContent = 'Ingredients: ' + recipe.ingredientLines.join(', ');
                 recipeCard.appendChild(recipeIngredients);
 
@@ -38,66 +40,66 @@ function searchRecipes(query) {
                 recipeCookingTime.textContent = 'Cooking Time: ' + recipe.totalTime + ' minutes';
                 recipeCard.appendChild(recipeCookingTime);
 
+                // Add the recipe card to the recipes list
                 recipesList.appendChild(recipeCard);
-
-                recipeName.addEventListener("click", function() {
-                    console.log("works");
-                    var descriptionArray = [recipe.cuisineType, recipe.dishType, recipe.mealType];
-                    window.localStorage.setItem("RecipeName", recipe.label);
-                    window.localStorage.setItem("RecipeImage", recipe.image);
-                    window.localStorage.setItem("RecipeIngredients", JSON.stringify(recipe.ingredients));
-                    window.localStorage.setItem("RecipeDescription", descriptionArray);
-                    window.localStorage.setItem("RecipeLink", recipe.url);
-                    window.localStorage.setItem("RecipeNutrients", JSON.stringify(recipe.digest));
-                    window.location.replace("recipe.html");
-                })
             });
         } else {
-            console.log("No recipes found.");
+            console.log("No recipes found."); // Console will log "No recipes found"
         }
     })
     .catch(error => {
-        console.log(`Request failed with error: ${error}`);
+        console.log(`Request failed with error: ${error}`); // Console will log "Request failed with error: error"
     });
 }
 
-document.getElementById('search-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const query = document.getElementById('query-input').value;
-    searchRecipes(query);
-});
-
 function initSavedItems(info) {
-    var saveContainer = document.querySelector("#saved-container");
+    var saveContainer = document.querySelector("#recipeCards");
     for (var i = 0; i < info.length; i++) {
         var savedName = info[i].foodName;
         var savedImg = info[i].foodImage;
 
         var createDiv = document.createElement("div");
-        createDiv.setAttribute("class", "saved-item");
+        createDiv.setAttribute("class", "column is-one-quarter");
+        
+        var cardDiv = document.createElement("div");
+        cardDiv.setAttribute("class", "card");
+
+        var cardImgDiv = document.createElement("div");
+        cardImgDiv.setAttribute("class", "card-image");
+
+        var cardImgFig = document.createElement("figure");
+        cardImgFig.setAttribute("class", "image is-4by3");
 
         var createImg = document.createElement("img");
         createImg.setAttribute("class", "saved-image");
 
-        var createInfoDiv = document.createElement("div");
-        createInfoDiv.setAttribute("class", "saved-info");
+        createImg.setAttribute("src", savedImg);
 
-        var createSaveTitle = document.createElement("h3");
-        createSaveTitle.setAttribute("class", "saved-header");
+        var contentDiv = document.createElement("div");
+        contentDiv.setAttribute("class", "card-content");
+
+        var contentTitle = document.createElement("p");
+        contentTitle.setAttribute("class", "title");
+        contentTitle.textContent = savedName;
+        var contentSub = document.createElement("p");
+        contentSub.setAttribute("class", "subtitle");
 
         var createButton = document.createElement('button');
         createButton.setAttribute("class", "saved-unsaved");
-        createButton.textContent = "Remove";
+        createButton.textContent = "X";
 
-        createSaveTitle.textContent = savedName;
-        createImg.setAttribute("src", savedImg);
-
-        createInfoDiv.appendChild(createSaveTitle);
-        createInfoDiv.appendChild(createButton);
-
-        createDiv.appendChild(createImg);
-        createDiv.appendChild(createInfoDiv);
+        cardImgFig.appendChild(createImg);
+        cardImgDiv.appendChild(cardImgFig);
         
+        contentDiv.appendChild(contentTitle);
+        contentDiv.appendChild(contentSub);
+        contentDiv.appendChild(createButton);
+
+        cardDiv.appendChild(cardImgDiv);
+        cardDiv.appendChild(contentDiv);
+
+        createDiv.appendChild(cardDiv);
+
         saveContainer.appendChild(createDiv);
     }
     saveContainer.addEventListener("click", function(event) {
@@ -154,3 +156,11 @@ function saveStuff() {
 }
 
 saveStuff();
+
+// Event listener for form submission
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+    const query = document.getElementById('query-input').value; // Get the query
+    searchRecipes(query); // Call the searchRecipes function
+});
+
